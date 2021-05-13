9094d2a49bf5223bea4510183156f534c4b98f69
Nikita Bruqus <bruqusg@gmail.com>
2021-05-13 23:43:13 +0300

    feat: add a database connection
    * add openDB fn
    * add 'dsn' flag

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type Config struct {
 	Addr string
 	StaticDir string
+	dsn string
 }
 
 type application struct {
@@ -21,10 +25,17 @@ func main() {
 	cfg := new(Config)
 	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&cfg.StaticDir, "static", "./ui/static/", "Path to static assets")
+	flag.StringVar(&cfg.dsn, "dsn", "web:test@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
+
+	db, err := openDB(cfg.dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+	defer db.Close()
 
 	app := &application{
 		infoLog: infoLog,
@@ -38,6 +49,17 @@ func main() {
 	}
 
 	infoLog.Printf("Starting Server on %s", cfg.Addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+	return db, nil
 }
