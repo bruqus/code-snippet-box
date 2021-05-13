851d341915e7f0df496733b1d2890add181eeeae
Nikita Bruqus <bruqusg@gmail.com>
2021-05-13 20:30:53 +0300

    feat: add command-line flags

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,31 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
+type Config struct {
+	Addr string
+	StaticDir string
+}
+
 func main() {
+	cfg := new(Config)
+	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address")
+	flag.StringVar(&cfg.StaticDir, "static", "./ui/static/", "Path to static assets")
+	flag.Parse()
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
 	mux.HandleFunc("/snippet/create", createSnippet)
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	fileServer := http.FileServer(http.Dir(cfg.StaticDir))
 	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
 
-	log.Println("Starting Server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting Server on %s", cfg.Addr)
+	err := http.ListenAndServe(cfg.Addr, mux)
 	log.Fatal(err)
 }
