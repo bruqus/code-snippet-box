ab0a395d52703d266a494485c5795872663eea1b
Nikita Bruqus <bruqusg@gmail.com>
2021-05-21 19:24:40 +0300

    feat(main.go): connect sessions
    * add sessions to application struct
    * add secret flag to config
    * initialize sessions

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -8,19 +8,23 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/golangcollege/sessions"
 )
 
 type Config struct {
 	Addr string
 	StaticDir string
 	dsn string
+	secret string
 }
 
 type application struct {
 	errorLog *log.Logger
 	infoLog *log.Logger
+	sessions *sessions.Session
 	snippets *mysql.SnippetModel
 	templateCache map[string]*template.Template
 }
@@ -30,6 +34,7 @@ func main() {
 	flag.StringVar(&cfg.Addr, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&cfg.StaticDir, "static", "./ui/static/", "Path to static assets")
 	flag.StringVar(&cfg.dsn, "dsn", "web:test@/snippetbox?parseTime=true", "MySQL data source name")
+	flag.StringVar(&cfg.secret, "secret", "s6Ndh+pPbnzHbS*+9Pk8qGWhTzbpa@ge", "Secret Key")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
@@ -46,9 +51,13 @@ func main() {
 		errorLog.Fatal(err)
 	}
 
+	session := sessions.New([]byte(cfg.secret))
+	session.Lifetime = 12 * time.Hour
+
 	app := &application{
 		infoLog: infoLog,
 		errorLog: errorLog,
+		sessions: session,
 		snippets: &mysql.SnippetModel{DB: db},
 		templateCache: templateCache,
 	}
