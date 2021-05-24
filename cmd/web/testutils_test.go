0eb7cc491edb5d153d29973875e39572f319d6b6
Nikita Bruqus <bruqusg@gmail.com>
2021-05-24 13:22:46 +0300

    test: add extractCSRFToken test util fn

--- cmd/web/testutils_test.go
+++ cmd/web/testutils_test.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"html"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/cookiejar"
 	"net/http/httptest"
+	"regexp"
 	"testing"
 	"time"
 
@@ -13,6 +15,8 @@ import (
 
 	"github.com/golangcollege/sessions"
 )
+
+var csrfTokenRX = regexp.MustCompile(`<input type="hidden" name="csrf_token" value=(.+)>`)
 
 func newTestApplication(t *testing.T) *application {
 	templateCache, err := newTemplateCache("./../../ui/html/")
@@ -69,4 +73,12 @@ func (ts *testServer) get(t *testing.T, urlPath string) (int, http.Header, []byt
 	}
 
 	return rs.StatusCode, rs.Header, body
+}
+
+func extractCSRFToken(t *testing.T, body []byte) string {
+	matches := csrfTokenRX.FindSubmatch(body)
+	if len(matches) < 2 {
+		t.Fatal("no csrf token found in body")
+	}
+	return html.UnescapeString(string(matches[1]))
 }
